56279dd821781d6ea1cae1819fcd756cd46368e8
Czarnodziej <pagodemc@gmail.com>
2013-09-08 17:59:53 +0200

    first commit, yea!

---
+++ app/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get( '/', function() {
+		return View::make( 'hello' );
+	} );
+
+Route::get( 'users', function() {
+		$users = User::all();
+
+		return View::make( 'users' )->with( 'users', $users );
+	} );
+
+Route::get('admin/logout',  array('as' => 'admin.logout',      'uses' => 'App\Controllers\Admin\AuthController@getLogout'));
+Route::get('admin/login',   array('as' => 'admin.login',       'uses' => 'App\Controllers\Admin\AuthController@getLogin'));
+Route::post('admin/login',  array('as' => 'admin.login.post',  'uses' => 'App\Controllers\Admin\AuthController@postLogin'));
+
+Route::group(array('prefix' => 'admin', 'before' => 'auth.admin'), function()
+{
+    Route::any('/',                'App\Controllers\Admin\PagesController@index');
+    Route::resource('articles',    'App\Controllers\Admin\ArticlesController');
+    Route::resource('pages',       'App\Controllers\Admin\PagesController');
+});
+
+    Route::any('pages', array(
+    'as' => 'admin.pages.index',
+    'uses' => 'App\Controllers\Admin\PagesController')
+    );
