217da6df0c30aa32bb66ad3fe6276cfe19ae95fa
Czarnodziej <pagodemc@gmail.com>
2013-09-14 15:59:51 +0200

    admin CRUD pt2

--- app/routes.php
+++ app/routes.php
@@ -11,28 +11,13 @@
 |
 */
 
-Route::get( '/', function() {
-		return View::make( 'hello' );
-	} );
-
-Route::get( 'users', function() {
-		$users = User::all();
-
-		return View::make( 'users' )->with( 'users', $users );
-	} );
-
-Route::get('admin/logout',  array('as' => 'admin.logout',      'uses' => 'App\Controllers\Admin\AuthController@getLogout'));
-Route::get('admin/login',   array('as' => 'admin.login',       'uses' => 'App\Controllers\Admin\AuthController@getLogin'));
-Route::post('admin/login',  array('as' => 'admin.login.post',  'uses' => 'App\Controllers\Admin\AuthController@postLogin'));
+Route::get('admin/logout', array('as' => 'admin.logout', 'uses' => 'App\Controllers\Admin\AuthController@getLogout'));
+Route::get('admin/login', array('as' => 'admin.login', 'uses' => 'App\Controllers\Admin\AuthController@getLogin'));
+Route::post('admin/login', array('as' => 'admin.login.post', 'uses' => 'App\Controllers\Admin\AuthController@postLogin'));
 
 Route::group(array('prefix' => 'admin', 'before' => 'auth.admin'), function()
 {
-    Route::any('/',                'App\Controllers\Admin\PagesController@index');
-    Route::resource('articles',    'App\Controllers\Admin\ArticlesController');
-    Route::resource('pages',       'App\Controllers\Admin\PagesController');
+Route::any('/', 'App\Controllers\Admin\PagesController@index');
+Route::resource('articles', 'App\Controllers\Admin\ArticlesController');
+Route::resource('pages', 'App\Controllers\Admin\PagesController');
 });
-
-    Route::any('pages', array(
-    'as' => 'admin.pages.index',
-    'uses' => 'App\Controllers\Admin\PagesController')
-    );
