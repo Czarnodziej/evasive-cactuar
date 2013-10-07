2d1f06d1f43a0ae352269868e665c562e077c214
Czarnodziej <pagodemc@gmail.com>
2013-10-07 17:04:05 +0200

    game.js draw the buttons and colors!

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -10,14 +10,16 @@
 // Draw a game space + style it:
 // - Isometric table 3x3
 // - start button
+// - cancel button
 // - total indicator
 // - speed indicator
+// - fail indicator
 
 // Start game
 // roll a random number (1-9) and set it as cell id
 // roll a random number and show either happy (positive, green) or sad face (negative, red) in previously set cell
 
-// Total = 15
+// Total = 0
 // Speed = 10 (a function of duration the positive/negative is shown)
 
 // Points:
@@ -31,6 +33,8 @@
 
 // Gameover:
 // if (points below 1) {
+//	game over!
+// else if (fail indicator = 0)
 //	game over!
 // }
 
@@ -50,7 +54,7 @@
 
 // Draw a table
 var data = ["1", "2", "3", "4", "5", "6", "7", "8", "9" ];
-var body = document.getElementById('game');
+var container = document.getElementById('game');
 var table = document.createElement("table");
 var i = 0;
 for (var r = 0; r < 3; r++) {
@@ -60,4 +64,76 @@ for (var r = 0; r < 3; r++) {
       cell.setAttribute("id", data[i++]);
   }
 }
-body.appendChild(table);
+container.appendChild(table);
+
+var startBtn = document.createElement("input");
+   startBtn.type = "submit";
+   startBtn.value = "Start";
+   startBtn.onclick = function() {
+        alert("ehehe");
+    };
+
+var cancelBtn = document.createElement("input");
+   cancelBtn.type = "submit";
+   cancelBtn.value = "Anuluj";
+   cancelBtn.onclick = function() {
+        alert("hiahia");
+    };
+
+container.appendChild(startBtn);
+container.appendChild(cancelBtn);
+
+
+// draw indicators
+
+var divTotal = document.createElement("div");
+	divTotal.id = "divTotal";
+	divTotal.className = "gameIndicatorDiv";
+	divTotal.innerHTML = "Punkty: ";
+
+var divSpeed = document.createElement("div");
+	divSpeed.id = "divSpeed";
+	divSpeed.className = "gameIndicatorDiv";
+	divSpeed.innerHTML = "Prędkość: ";
+
+var divFail = document.createElement("div");
+	divFail.id = "divFail";
+	divFail.className = "gameIndicatorDiv";
+	divFail.innerHTML = "Szanse: ";
+
+var spanTotal = document.createElement("span");
+	spanTotal.id = "Total";
+	spanTotal.className = "gameSpan";
+
+var spanSpeed = document.createElement("span");
+	spanSpeed.id = "Speed";
+	spanSpeed.className = "gameSpan";
+
+var spanFail = document.createElement("span");
+	spanFail.id = "Fail";
+	spanFail.className = "gameSpan";
+
+container.appendChild(divTotal);
+container.appendChild(divSpeed);
+container.appendChild(divFail);
+
+divTotal.appendChild(spanTotal);
+divSpeed.appendChild(spanSpeed);
+divFail.appendChild(spanFail);
+
+//default values of indicators
+
+spanTotal.innerHTML = 0;
+spanSpeed.innerHTML = 10;
+spanFail.innerHTML = 5;
+
+// test the colors!
+document.getElementById('1').className = "gameCellRed";
+document.getElementById('2').className = "gameCellGreen";
+document.getElementById('3').className = "gameCellRed";
+document.getElementById('4').className = "gameCellGreen";
+document.getElementById('5').className = "gameCellRed";
+document.getElementById('6').className = "gameCellGreen";
+document.getElementById('7').className = "gameCellRed";
+document.getElementById('8').className = "gameCellGreen";
+document.getElementById('9').className = "gameCellRed";
