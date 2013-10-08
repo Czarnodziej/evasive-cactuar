37bb7bedd9fccf3cad7c64f52168971b0ba59c34
Czarnodziej <pagodemc@gmail.com>
2013-10-08 17:06:48 +0200

    game logic start

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -52,7 +52,7 @@
 // }
 
 
-// Draw a table
+// draw a table
 var data = ["1", "2", "3", "4", "5", "6", "7", "8", "9" ];
 var container = document.getElementById('game');
 var table = document.createElement("table");
@@ -66,25 +66,22 @@ for (var r = 0; r < 3; r++) {
 }
 container.appendChild(table);
 
+// draw buttons todo: use objects
+
 var startBtn = document.createElement("input");
    startBtn.type = "submit";
    startBtn.value = "Start";
-   startBtn.onclick = function() {
-        alert("ehehe");
-    };
 
 var cancelBtn = document.createElement("input");
    cancelBtn.type = "submit";
    cancelBtn.value = "Anuluj";
-   cancelBtn.onclick = function() {
-        alert("hiahia");
-    };
+
 
 container.appendChild(startBtn);
 container.appendChild(cancelBtn);
 
 
-// draw indicators
+// draw indicators todo: use objects
 
 var divTotal = document.createElement("div");
 	divTotal.id = "divTotal";
@@ -121,19 +118,66 @@ divTotal.appendChild(spanTotal);
 divSpeed.appendChild(spanSpeed);
 divFail.appendChild(spanFail);
 
-//default values of indicators
 
-spanTotal.innerHTML = 0;
-spanSpeed.innerHTML = 10;
-spanFail.innerHTML = 5;
+
+
+
+
+
+
+
+
+
+//default values of indicators todo: replace innerHTML with textNodes
+
+var totalCounter = 0;
+var speedCounter = 10;
+var failCounter = 5;
+
+spanTotal.innerHTML = totalCounter;
+spanSpeed.innerHTML = speedCounter;
+spanFail.innerHTML = failCounter;
+
 
 // test the colors!
-document.getElementById('1').className = "gameCellRed";
-document.getElementById('2').className = "gameCellGreen";
-document.getElementById('3').className = "gameCellRed";
-document.getElementById('4').className = "gameCellGreen";
-document.getElementById('5').className = "gameCellRed";
-document.getElementById('6').className = "gameCellGreen";
-document.getElementById('7').className = "gameCellRed";
-document.getElementById('8').className = "gameCellGreen";
-document.getElementById('9').className = "gameCellRed";
+// document.getElementById('1').className = "gameCellRed";
+// document.getElementById('2').className = "gameCellGreen";
+// document.getElementById('3').className = "gameCellRed";
+// document.getElementById('4').className = "gameCellGreen";
+// document.getElementById('5').className = "gameCellRed";
+// document.getElementById('6').className = "gameCellGreen";
+// document.getElementById('7').className = "gameCellRed";
+// document.getElementById('8').className = "gameCellGreen";
+// document.getElementById('9').className = "gameCellRed";
+
+
+// logic
+
+        var color = Math.floor(Math.random()*2); // one of 2 possible colors
+        var counter = Math.floor(Math.random()*10)+1; // one of 10 possible colors (1-10)
+        var d = document.getElementById(counter); // assign table cell to variable
+
+   startBtn.onclick = function() {
+
+ setTimeout(function() {
+
+        if (color === 1)
+        {
+		d.className += ' gameCellRed';
+		}
+		else
+		{
+		d.className += ' gameCellGreen';
+		}
+
+ }, 1500);
+
+    };
+
+
+
+
+
+    cancelBtn.onclick = function() {
+         alert("hiahia");
+     };
