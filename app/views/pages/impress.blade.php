97e616f5d1ea6ddc57d962bbf7bc030eb41130ea
Czarnodziej <pagodemc@gmail.com>
2014-02-12 14:03:51 +0100

    piwik integration

--- app/views/pages/impress.blade.php
+++ app/views/pages/impress.blade.php
@@ -105,6 +105,21 @@
         </script>
         <script src="{{ asset('assets/js/vendor/impress.min.js') }}"></script>
         <script>impress().init();</script>
+        <!-- Piwik -->
+        <script type="text/javascript">
+          var _paq = _paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.linuxpl.com//";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', 7086]);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+            g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+          })();
 
+        </script>
+        <noscript><p><img src="http://piwik.linuxpl.com/piwik.php?idsite=7086" style="border:0" alt="" /></p></noscript>
+        <!-- End Piwik Code -->
     </body>
 </html>
